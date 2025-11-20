77e747b4dcf0f165e9ee6012b8ca121670f417eb
Sidney Muhammad <info@sidneym.com>
2025-11-20 14:02:39 -0600

    Corrected port

--- main.py
+++ main.py
@@ -1081,4 +1081,6 @@ async def health_check():
 
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run(app, host="0.0.0.0", port=8000)
+    import os
+    port = int(os.getenv("PORT", 8080))
+    uvicorn.run(app, host="0.0.0.0", port=port)
